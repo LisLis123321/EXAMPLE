48f0931e7bb4f58e2857bc518be811bf407b0743
User1223 <hitrovaleks2014@gmail.com>
2023-03-03 16:33:43 +0300

    initial commit 2

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+            long balance = 2_000_000_000;
+            long transfer1 = 500_000_000;
+            long total = balance + transfer1;
+            System.out.println(total);
+        }
+    }
+}
