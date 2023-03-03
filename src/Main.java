c5f729f695c1a52784b48fbbe6ca6dcd6d02ac54
User1223 <hitrovaleks2014@gmail.com>
2023-03-03 15:47:51 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
        long balance = 2_000_000_000;
-       long transfer = 500_000_000;
-       long total = balance + transfer;
+       long transfer1 = 500_000_000;
+       long total = balance + transfer1;
        System.out.println(total);
     }
 }
